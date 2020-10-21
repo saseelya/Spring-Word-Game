b2b059d0455642e872d2a353e7eb63437d9d76f5
Brian Sanner <sanner@dev-10.com>
2020-10-21 15:43:24 -0500

    Finalized product.

--- src/main/java/learn/game/GameController.java
+++ src/main/java/learn/game/GameController.java
@@ -10,13 +10,21 @@ import java.util.ArrayList;
 @RestController
 public class GameController {
 
-    private final String wordTheyGuess = "The Priest";
+    private final String wordTheyGuess = "Saltine SparkNotes";
     ArrayList<String> correctLetters = new ArrayList<>();
     ArrayList<String> incorrectLetters = new ArrayList<>();
     Temp temp = new Temp(wordTheyGuess);
 
     @PutMapping("/guess/{letter}")
     public String guessLetter(@PathVariable String letter) {
+        if (letter == null || letter.length() != 1){
+            return getIncorrectLetters() + "\nErr: Please enter one and only one letter.";
+        }
+        if (incorrectLetters.contains(letter.toLowerCase()) || correctLetters.contains(letter.toLowerCase())){
+            return getIncorrectLetters() + "\nErr: You've already guessed that letter.";
+        }
+
+
         if (wordTheyGuess.toLowerCase().contains(letter.toLowerCase())) {
             correctLetters.add(letter.toLowerCase());
             temp.updateTemp(letter.toLowerCase(), wordTheyGuess);
@@ -24,7 +32,7 @@ public class GameController {
         else {
             incorrectLetters.add(letter.toLowerCase());
         }
-        return temp.getTemp().toString();
+        return getIncorrectLetters();
     }
 
     @GetMapping("/word")
@@ -32,5 +40,110 @@ public class GameController {
         return temp.getTemp().toString();
     }
 
+    private String getIncorrectLetters(){
+        StringBuilder printedLetters = new StringBuilder();
+        for (String s : incorrectLetters){
+            printedLetters.append(s).append(" ");
+        }
+        printedLetters.append("\n").append(getDiagram());
+        return printedLetters.append(temp.getTemp().toString()).toString();
+    }
+
+    private String getDiagram(){
+        String diagram = "";
+        switch (incorrectLetters.size()){
+            case 0:
+                diagram +=
+                        "_______\n" +
+                        " |    |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "      |\n" +
+                        "______|__\n";
+                break;
+            case 1:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                "      |\n" +
+                                "      |\n" +
+                                "______|__\n";
+                break;
+            case 2:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                " |    |\n" +
+                                "      |\n" +
+                                "______|__\n";
+                break;
+            case 3:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                " |\\   |\n" +
+                                "      |\n" +
+                                "______|__\n";
+                break;
+            case 4:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                "/|\\   |\n" +
+                                "      |\n" +
+                                "______|__\n";
+                break;
+            case 5:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                "/|\\   |\n" +
+                                "  \\   |\n" +
+                                "______|__\n";
+                break;
+            case 6:
+                diagram +=
+                        "_______\n" +
+                                " |    |\n" +
+                                " O    |\n" +
+                                "/|\\   |\n" +
+                                "/ \\   |\n" +
+                                "______|__\n";
+                break;
+            default:
+                diagram += "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀\n" +
+                        "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼\n" +
+                        "██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀\n" +
+                        "██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼\n" +
+                        "███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼\n" +
+                        "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼\n" +
+                        "██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼\n" +
+                        "██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼\n" +
+                        "███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼██┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼████▄┼┼┼▄▄▄▄▄▄▄┼┼┼▄████┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼▀▀█▄█████████▄█▀▀┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼█████████████┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼██▀▀▀███▀▀▀██┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼██┼┼┼███┼┼┼██┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼█████▀▄▀█████┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼███████████┼┼┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼▄▄▄██┼┼█▀█▀█┼┼██▄▄▄┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼▀▀██┼┼┼┼┼┼┼┼┼┼┼██▀▀┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼▀▀┼┼┼┼┼┼┼┼┼┼┼\n" +
+                        "┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼\n";
+                break;
+        }
+        return diagram;
+    }
 
 }
